ec99d2d883c31e24fc76623e6fe4aab728f11c9b
Piotr Sarna <piotr@sarna.dev>
2025-12-16 10:05:48 +0100

    abstract registry

--- pkg/reg/reg.go
+++ pkg/reg/reg.go
@@ -10,6 +10,7 @@ import (
 	"strings"
 	"time"
 
+	v4 "github.com/aws/aws-sdk-go-v2/aws/signer/v4"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/gorilla/mux"
@@ -18,13 +19,17 @@ import (
 	v1 "github.com/opencontainers/image-spec/specs-go/v1"
 )
 
-type Handler struct {
+type Registry struct {
 	s3Client *s3.Client
 	bucket   string
 	db       *RegistryDB
 }
 
-func NewRouter(ctx context.Context, bucket string) (*mux.Router, error) {
+type Handler struct {
+	registry *Registry
+}
+
+func NewRegistry(ctx context.Context, bucket string) (*Registry, error) {
 	cfg, err := config.LoadDefaultConfig(ctx)
 	if err != nil {
 		return nil, fmt.Errorf("unable to load SDK config, %v", err)
@@ -36,10 +41,21 @@ func NewRouter(ctx context.Context, bucket string) (*mux.Router, error) {
 		return nil, fmt.Errorf("failed to initialize database: %w", err)
 	}
 
-	h := &Handler{
+	return &Registry{
 		s3Client: s3Client,
 		bucket:   bucket,
 		db:       db,
+	}, nil
+}
+
+func NewRouter(ctx context.Context, bucket string) (*mux.Router, error) {
+	registry, err := NewRegistry(ctx, bucket)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create registry: %w", err)
+	}
+
+	h := &Handler{
+		registry: registry,
 	}
 
 	r := mux.NewRouter()
@@ -110,14 +126,10 @@ func (h *Handler) checkAPISupport(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 }
 
-func (h *Handler) getBlob(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	name := vars["name"]
-	digest := vars["digest"]
+func (r *Registry) getBlobRedirect(ctx context.Context, name string, digest string, method string) (string, error) {
 	algo, hex, found := strings.Cut(digest, ":")
 	if !found {
-		http.Error(w, "Invalid digest format", http.StatusBadRequest)
-		return
+		return "", fmt.Errorf("invalid digest format")
 	}
 
 	blobKey := fmt.Sprintf("docker/registry/v2/blobs/%s/%s/%s/data", algo, hex[0:2], hex)
@@ -127,48 +139,54 @@ func (h *Handler) getBlob(w http.ResponseWriter, r *http.Request) {
 
 	expires := 15 * time.Minute
 
-	presignClient := s3.NewPresignClient(h.s3Client)
-	switch r.Method {
+	var err error
+	var presignedReq *v4.PresignedHTTPRequest
+	presignClient := s3.NewPresignClient(r.s3Client)
+	switch method {
 	case http.MethodGet:
-		presignedReq, err := presignClient.PresignGetObject(r.Context(),
+		presignedReq, err = presignClient.PresignGetObject(ctx,
 			&s3.GetObjectInput{
-				Bucket: &h.bucket,
+				Bucket: &r.bucket,
 				Key:    &blobKey,
 			},
 			s3.WithPresignExpires(expires),
 		)
-		if err != nil {
-			http.Error(w, fmt.Sprintf("failed to create presigned URL: %v", err), http.StatusInternalServerError)
-			return
-		}
-		http.Redirect(w, r, presignedReq.URL, http.StatusFound)
-
 	case http.MethodHead:
-		presignedReq, err := presignClient.PresignHeadObject(r.Context(),
+		presignedReq, err = presignClient.PresignHeadObject(ctx,
 			&s3.HeadObjectInput{
-				Bucket: &h.bucket,
+				Bucket: &r.bucket,
 				Key:    &blobKey,
 			},
 			s3.WithPresignExpires(expires),
 		)
-		if err != nil {
-			http.Error(w, fmt.Sprintf("failed to create presigned URL: %v", err), http.StatusInternalServerError)
-			return
-		}
-
-		http.Redirect(w, r, presignedReq.URL, http.StatusFound)
-
 	default:
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return "", fmt.Errorf("Method not allowed: %s", http.StatusMethodNotAllowed)
 	}
+	if err != nil {
+		return "", fmt.Errorf("failed to create presigned URL: %w", err)
+	}
+	return presignedReq.URL, nil
 }
 
-func (h *Handler) getManifestSHA(ctx context.Context, repo string, tag string) (digest.Digest, error) {
+func (h *Handler) getBlob(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	name := vars["name"]
+	digest := vars["digest"]
+	presignedURL, err := h.registry.getBlobRedirect(r.Context(), name, digest, r.Method)
+	if err != nil {
+		slog.Error("error getting blob redirect", "error", err)
+		http.Error(w, fmt.Sprintf("error getting blob redirect: %v", err), http.StatusInternalServerError)
+		return
+	}
+	http.Redirect(w, r, presignedURL, http.StatusFound)
+}
+
+func (r *Registry) getManifestSHA(ctx context.Context, repo string, tag string) (digest.Digest, error) {
 	metaKey := fmt.Sprintf("docker/registry/v2/repositories/%s/_manifests/tags/%s/current/link", repo, tag)
 	slog.Debug("getting manifest SHA", "repo", repo, "tag", tag, "metaKey", metaKey)
 
-	obj, err := h.s3Client.GetObject(ctx, &s3.GetObjectInput{
-		Bucket: &h.bucket,
+	obj, err := r.s3Client.GetObject(ctx, &s3.GetObjectInput{
+		Bucket: &r.bucket,
 		Key:    &metaKey,
 	})
 	if err != nil {
@@ -182,68 +200,62 @@ func (h *Handler) getManifestSHA(ctx context.Context, repo string, tag string) (
 	return digest.Parse(string(sha))
 }
 
+func (r *Registry) getManifest(ctx context.Context, name string, reference string) (*v1.Manifest, []byte, error) {
+	readyManifestBytes, err := r.db.GetManifest(name, reference)
+	if err == nil {
+		var manifest v1.Manifest
+		if err := json.Unmarshal([]byte(readyManifestBytes), &manifest); err != nil {
+			return nil, nil, err
+		}
+		return &manifest, []byte(readyManifestBytes), nil
+	}
+
+	sha, err := r.getManifestSHA(ctx, name, reference)
+	if err != nil {
+		return nil, nil, err
+	}
+	hex := sha.Hex()
+	blobKey := fmt.Sprintf("docker/registry/v2/blobs/sha256/%s/%s/data", hex[0:2], hex)
+	slog.Debug("getting manifest blob", "blobKey", blobKey)
+	obj, err := r.s3Client.GetObject(ctx, &s3.GetObjectInput{
+		Bucket: &r.bucket,
+		Key:    &blobKey,
+	})
+	if err != nil {
+		return nil, nil, err
+	}
+	defer obj.Body.Close()
+	blobData, err := io.ReadAll(obj.Body)
+	if err != nil {
+		return nil, nil, err
+	}
+	var manifest v1.Manifest
+	if err := json.Unmarshal(blobData, &manifest); err != nil {
+		return nil, nil, err
+	}
+
+	if err := r.db.PutManifest(name, reference, string(blobData), &manifest); err != nil {
+		slog.Error("error storing manifest in database", "error", err)
+	}
+
+	return &manifest, blobData, nil
+}
+
 func (h *Handler) getManifest(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	name := vars["name"]
 	reference := vars["reference"]
 
-	readyManifestBytes, err := h.db.GetManifest(name, reference)
-	if err == nil {
-		var manifest v1.Manifest
-		if err := json.Unmarshal([]byte(readyManifestBytes), &manifest); err != nil {
-			slog.Error("error unmarshalling manifest", "error", err)
-			http.Error(w, fmt.Sprintf("error unmarshalling manifest: %v", err), http.StatusInternalServerError)
-			return
-		}
-		w.Header().Set("Content-Type", manifest.MediaType)
-		_, err = w.Write([]byte(readyManifestBytes))
-		if err != nil {
-			slog.Error("error writing manifest response", "error", err)
-			http.Error(w, fmt.Sprintf("error writing manifest response: %v", err), http.StatusInternalServerError)
-		}
-		return
-	}
-
-	sha, err := h.getManifestSHA(r.Context(), name, reference)
+	manifest, manifestBytes, err := h.registry.getManifest(r.Context(), name, reference)
 	if err != nil {
-		slog.Error("error getting manifest SHA", "error", err)
+		slog.Error("error getting manifest", "error", err)
 		http.Error(w, fmt.Sprintf("error getting manifest: %v", err), http.StatusInternalServerError)
 		return
 	}
-	hex := sha.Hex()
-	blobKey := fmt.Sprintf("docker/registry/v2/blobs/sha256/%s/%s/data", hex[0:2], hex)
-	slog.Debug("getting manifest blob", "blobKey", blobKey)
-	obj, err := h.s3Client.GetObject(r.Context(), &s3.GetObjectInput{
-		Bucket: &h.bucket,
-		Key:    &blobKey,
-	})
-	if err != nil {
-		slog.Error("error getting manifest blob", "error", err)
-		http.Error(w, fmt.Sprintf("error getting manifest blob: %v", err), http.StatusInternalServerError)
-		return
-	}
-	defer obj.Body.Close()
-	blobData, err := io.ReadAll(obj.Body)
-	if err != nil {
-		slog.Error("error reading manifest blob", "error", err)
-		http.Error(w, fmt.Sprintf("error reading manifest blob: %v", err), http.StatusInternalServerError)
-		return
-	}
-	var manifest v1.Manifest
-	if err := json.Unmarshal(blobData, &manifest); err != nil {
-		slog.Error("error unmarshalling manifest", "error", err)
-		http.Error(w, fmt.Sprintf("error unmarshalling manifest: %v", err), http.StatusInternalServerError)
-		return
-	}
-
-	if err := h.db.PutManifest(name, reference, string(blobData), &manifest); err != nil {
-		slog.Error("error storing manifest in database", "error", err)
-	}
 
 	w.Header().Set("Content-Type", manifest.MediaType)
-	w.Header().Set("Docker-Content-Digest", string(sha))
-	w.Header().Set("Content-Length", fmt.Sprintf("%d", len(blobData)))
-	_, err = w.Write(blobData)
+	w.Header().Set("Content-Length", fmt.Sprintf("%d", len(manifestBytes)))
+	_, err = w.Write(manifestBytes)
 	if err != nil {
 		slog.Error("error writing manifest response", "error", err)
 		http.Error(w, fmt.Sprintf("error writing manifest response: %v", err), http.StatusInternalServerError)
@@ -305,63 +317,54 @@ type tags struct {
 	Tags []string `json:"tags"`
 }
 
-func (h *Handler) listTags(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	name := vars["name"]
-
-	readyTags, err := h.db.ListTags(name)
-	if err == nil {
-		slog.Debug("retrieved tags from database", "name", name, "tags", readyTags)
-		marshaledTags, err := json.Marshal(tags{
-			Name: name,
-			Tags: readyTags,
-		})
-		if err != nil {
-			slog.Error("error marshalling tags", "error", err)
-			http.Error(w, fmt.Sprintf("error marshalling tags: %v", err), http.StatusInternalServerError)
-			return
-		}
-		_, err = w.Write(marshaledTags)
-		if err != nil {
-			slog.Error("error writing tags response", "error", err)
-			http.Error(w, fmt.Sprintf("error writing tags response: %v", err), http.StatusInternalServerError)
-		}
-		return
+func (r *Registry) listTags(ctx context.Context, name string) ([]string, error) {
+	readyTags, err := r.db.ListTags(name)
+	if err == nil && len(readyTags) > 0 {
+		return readyTags, nil
 	}
 
 	var repoTags []string
 	var continuationToken *string
 	prefix := fmt.Sprintf("docker/registry/v2/repositories/%s/_manifests/tags/", name)
 	for {
-		req, err := h.s3Client.ListObjectsV2(r.Context(), &s3.ListObjectsV2Input{
-			Bucket:            &h.bucket,
+		req, err := r.s3Client.ListObjectsV2(ctx, &s3.ListObjectsV2Input{
+			Bucket:            &r.bucket,
 			Prefix:            &prefix,
 			ContinuationToken: continuationToken,
 		})
 		if err != nil {
-			slog.Error("error listing S3 objects", "error", err)
-			http.Error(w, fmt.Sprintf("error listing S3 objects: %v", err), http.StatusInternalServerError)
-			return
+			return nil, err
 		}
 
-		// Process results from this page
 		for _, obj := range req.Contents {
 			if strings.HasSuffix(*obj.Key, "current/link") {
 				tag := strings.TrimSuffix(strings.TrimPrefix(*obj.Key, fmt.Sprintf("docker/registry/v2/repositories/%s/_manifests/tags/", name)), "/current/link")
 				repoTags = append(repoTags, tag)
 			}
 		}
-
-		// Break if no more pages, otherwise continue with next token
 		if req.IsTruncated == nil || !*req.IsTruncated {
 			break
 		}
 		continuationToken = req.NextContinuationToken
 	}
 
-	err = h.db.PutTags(name, repoTags)
+	err = r.db.PutTags(name, repoTags)
 	if err != nil {
 		slog.Error("error storing tags in database", "error", err)
+	}
+
+	return repoTags, nil
+}
+
+func (h *Handler) listTags(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	name := vars["name"]
+
+	repoTags, err := h.registry.listTags(r.Context(), name)
+	if err != nil {
+		slog.Error("error listing tags", "error", err)
+		http.Error(w, fmt.Sprintf("error listing tags: %v", err), http.StatusInternalServerError)
+		return
 	}
 
 	marshaledTags, err := json.Marshal(tags{
